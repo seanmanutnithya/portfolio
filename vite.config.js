2ab115381b96b7191b0620cc7692b9865d20d9b2
wee08 <227932670+seanmanutnithya@users.noreply.github.com>
2026-09-25 17:12:34 +0700

    install wrangler and deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cloudflare()],
   resolve: {
     alias: { "@": path.resolve(import.meta.dirname, "src") },
   },
